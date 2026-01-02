0e478677a7ef75b2ccb6d2a186918f0879116dcf
Rafe Ng <rafeng.rn@gmail.com>
2026-01-03 01:05:07 +0800

    Completed the constellation_detector enddpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import io
 import os
+import cv2
 import base64
+import numpy as np
 from ultralytics import YOLO
 from typing import List, Optional, Dict, Any
 
@@ -13,7 +15,8 @@ app = FastAPI()
 
 
 class DetectorResponse(BaseModel):
-    yolo_result: List[Any]
+    yolo_img_result: str
+    yolo_class_result: List[str, Any]
 
 class ExplainerResponse(BaseModel):
     llm_result: List[Any]
@@ -30,6 +33,9 @@ def file_to_pil(uploaded_file):
     img = Image.open(uploaded_file).convert("RGB")
     return img
 
+def encoded_img(img: np.array) -> str:
+    _, buffer = cv2.imencode(".jpg", img)
+    return base64.b64encode(buffer).decode("utf-8")
 
 
 @app.post("/constellation_detector", response_model=DetectorResponse)
@@ -38,7 +44,20 @@ async def constellation_detector(files: List[UploadFile] = File(...)):
         img = file_to_pil(files[0])
         results = constellation_detectoer_model(img)
 
-        return
+        class_name_list = []
+
+        boxes = results[0].boxes
+        for box in boxes:
+            class_id = int(box.cls[0])
+            class_name = constellation_detectoer_model.names[class_id]
+            class_name_list.append(class_name)
+            
+        img_result = encoded_img(results[0].plot())
+
+        return DetectorResponse(
+            yolo_img_result=img_result,
+            yolo_class_result=class_name_list
+        )
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e)) 
