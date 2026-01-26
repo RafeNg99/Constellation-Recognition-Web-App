d1c67d58890a74f2e9d32bcdaee19635e5880047
Rafe Ng <rafeng.rn@gmail.com>
2026-01-26 23:00:13 +0800

    Removed config.py commented line

--- app/config.py
+++ app/config.py
@@ -4,7 +4,5 @@ import os
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
 URL = f"{OLLAMA_BASE_URL}/api/chat"
 
-# URL = "http://ollama:11434/v1/chat/completions"
-
 # LLM model name
 MODEL_NAME = "qwen3:4b"
